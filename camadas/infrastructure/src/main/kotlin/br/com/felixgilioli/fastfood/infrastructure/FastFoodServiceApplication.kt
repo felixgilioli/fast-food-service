9fc6da243b3babe03f41c01efb6116afc6fb64b0
Felix <felix_gilioli@hotmail.com>
2025-08-04 00:47:43 -0300

    Swagger application info

--- camadas/infrastructure/src/main/kotlin/br/com/felixgilioli/fastfood/infrastructure/FastFoodServiceApplication.kt
+++ camadas/infrastructure/src/main/kotlin/br/com/felixgilioli/fastfood/infrastructure/FastFoodServiceApplication.kt
@@ -1,8 +1,17 @@
 package br.com.felixgilioli.fastfood.infrastructure
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+	info = Info(
+		title = "FastFood Service API",
+		version = "v1",
+		description = "API para gerenciamento do serviço FastFood"
+	)
+)
 @SpringBootApplication
 class FastFoodServiceApplication
 
